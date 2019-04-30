8be7a126396a34b7ff13d4803c2dbb700c4b94b3
Vytautas Vasiliauskas <vytautas1983@gmail.com>
2019-04-30 12:54:26 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Welcome to React Native! </Text>
+        <Text >To get started, edit App.js</Text>
+        <Text ></Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
